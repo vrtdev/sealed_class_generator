83a46346636205972a60269bc011741c9090a23f
Tim Rijckaert <timrijckaert@users.noreply.github.com>
2019-11-23 16:18:00 +0100

    👌 Type List instead of Type Parameters

--- sealed_class_generator/lib/src/gen.dart
+++ sealed_class_generator/lib/src/gen.dart
@@ -6,8 +6,8 @@ import 'package:sealed_class/sealed_class.dart';
 import 'package:source_gen/source_gen.dart';
 
 import 'checker.dart';
-import 'transformer.dart';
 import 'printer.dart';
+import 'transformer.dart';
 import 'visitor.dart';
 
 class SealedGenerator extends GeneratorForAnnotation<Sealed> {
@@ -17,13 +17,20 @@ class SealedGenerator extends GeneratorForAnnotation<Sealed> {
     ConstantReader annotation,
     BuildStep buildStep,
   ) {
+    final typeParams = annotation.objectValue
+        .getField("types")
+        .toListValue()
+        .map((dartObj) => dartObj.toTypeValue())
+        .map((type) => type.name)
+        .toList();
+
     final visitor = SealedClassVisitor();
     element.visitChildren(visitor);
 
-    Checker.checkInputValidity(visitor.className, visitor.typeParams);
+    Checker.checkInputValidity(visitor.className, typeParams);
 
     final typeParameterData =
-        TypeParameterTransformer.toGeneratedCodeData(visitor.typeParams);
+        TypeParameterTransformer.toGeneratedCodeData(typeParams);
     final printerOutput =
         Printer.constructOutput(visitor.className, typeParameterData);
 
